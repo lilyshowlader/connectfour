835db195c11e8c23a4e245ec502c94f1602e168d
lilyshowlader <lilyshowlader@gmail.com>
2022-09-12 18:12:40 -0400

    add confetti and time

--- js/app.js
+++ js/app.js
@@ -97,10 +97,12 @@ function render() {
     displayMessage.textContent = "congrats player one, you won!"
     const helluvaParty = new Audio("../assets/audio/Helluva Party (2).mp3")
     helluvaParty.play()
+    confetti.start(22000)
   } else if (winner === -1) {
     displayMessage.textContent = "congrats player two, you won!"
     const helluvaParty = new Audio("../assets/audio/Helluva Party (2).mp3")
     helluvaParty.play()
+    confetti.start(22000)
   } else if (winner === 'T') {
     displayMessage.textContent = "uh oh, let's try again"
   } 
